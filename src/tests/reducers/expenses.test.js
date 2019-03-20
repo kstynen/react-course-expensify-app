ffca0096b15203610e6b6333f618d7016b2f52f9
Koen Stynen <koenstynen@MacBook-Pro-van-Koen.local>
2019-03-20 19:24:43 +0100

    Setup fetching expenses

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -68,3 +68,13 @@ test('should not edit an expense if expense not found', () => {
   expect(state).toEqual(expenses);
 });
 
+test('should set expenses', () => {
+  //const expenses = expenses;
+  const action = {
+    type: 'SET_EXPENSES', 
+    expenses: [expenses[1]]
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([expenses[1]]);
+});
+
